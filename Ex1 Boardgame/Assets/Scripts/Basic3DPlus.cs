2330e7906b515c6d55361fad8261f09883bb3757
courtney Ramos <Courtneyramos65@gmail.com>
2017-09-12 23:50:53 -0400

    GriddedBoardStyle
    player cmove on the x,yand z

---
+++ Ex1 Boardgame/Assets/Scripts/Basic3DPlus.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic3DPlus : MonoBehaviour
+{
+
+
+    public class Basic3D : MonoBehaviour
+    {
+        public GameObject mover;
+
+        public float moveSpeed;
+        public Vector3 startingPosition;
+
+
+        // Use this for initialization
+
+        void Start()
+        
+        {
+            startingPosition = mover.transform.position;
+            moveSpeed = 0.5f;
+
+        }
+
+
+
+        // Update is called once per frame
+
+        void Update()
+        {
+            if (mover.transform.position.z < 0) {
+            }
+
+            transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical"));//this code watch a youtube video for//
+        }
+    }
+
+}

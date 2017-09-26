4bc6398cd3d4ec0a3dec68bc7e2b5d4b1086522d
courtney Ramos <Courtneyramos65@gmail.com>
2017-09-26 06:56:00 -0400

    player can move
    player is able to move and there some constraints on the player moveement it is also some bumbs place on the board if the player hits a bomb the player poistion will rest to the checkpoint

---
+++ Ex1 Boardgame/Assets/Scripts/Falldetect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falldetect : MonoBehaviour {
+    Vector3 checkpoint;
+	// Use this for initialization
+	void Start () {
+        checkpoint = new Vector3(-6, 0, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < -10)
+        { transform.position=checkpoint;
+
+        }
+	}
+}

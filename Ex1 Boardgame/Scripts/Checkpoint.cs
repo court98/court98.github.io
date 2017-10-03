22008ebff0ba5ba06d27ce1ae80a5a6118a43bbc
courtney Ramos <Courtneyramos65@gmail.com>
2017-10-03 07:34:17 -0400

    changes made to repository
    New Assets such as aduioclips and script added. enemies are also incorporated along with a script

---
+++ Ex1 Boardgame/Scripts/Checkpoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Vector3 checkpoint;
+    public Transform player;
+    private GameObject bomb;
+
+    // Use this for initialization
+    void Start()
+    {
+        player.transform.position= new Vector3(7, 1, -7);
+        checkpoint = new Vector3(-7, 1, -8);
+    }   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag==("Bomb"))
+        {
+            player.transform.position = checkpoint;
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
---
+++ Ex1 Boardgame/Scripts/PlayClip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayClip : MonoBehaviour {
+    public AudioClip Thump;
+    public AudioClip BombSound;
+    AudioSource audiosource;
+
+    // Use this for initialization
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+
+
+    }
+    void OnCollisionEnter()
+    {
+        audioSource.PlayOneShot(Thump, 0.F);
+    }
+}
+    void OnCollisionEnter()
+    {
+        audioSource.PlayOneShot(BombSound, 0.7F);
+    }
+}
+
+// Update is called once per frame
+void Update () {
+		
+	}
+}
+
